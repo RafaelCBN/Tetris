4b1b878b38b357816dda91f5e5ddcb406e010685
reistill <joelreistill@gmail.com>
2025-02-24 21:06:14 -0300

    add linhas

--- ProjetoPY.py
+++ ProjetoPY.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.locals import * #submodolo
 from sys import exit
+import random
 
 pygame.init() #inicializar as funcoes e variaveis do pygame
 
 largura = 500
 altura = 1000
-cor = 255,0,0
+local_objeto = 200,300
+
 tela = pygame.display.set_mode((largura,altura)) #largura e altura da tela
 pygame.display.set_caption('Tetris')
 while True: #loop
@@ -14,6 +16,12 @@ while True: #loop
         if event.type == QUIT:
             pygame.quit()
             exit()
+    cor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))    
+    #-----------------------------X---Y---L--A)
+    pygame.draw.rect(tela,(cor),(400,0,1,2000))
+    #pygame.draw.rect(tela,(cor),(200,300,100,5))#Reta esquerda
+    #pygame.draw.rect(tela,(cor),(300,300,5,100))#Reta esquerda
+    #pygame.draw.rect(tela,(cor),(200,300,5,100))#Teto
+    #pygame.draw.rect(tela,(cor),(200,400,100,5))#Chao
 
-    pygame.draw.rect(tela,(cor),(200,300,5,100))
     pygame.display.update()

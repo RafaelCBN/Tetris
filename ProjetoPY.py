9861a2fade1b7fbce15ccb4fbc089a8df418309e
MateusDalligna <mateusdalligna.ifc@gmail.com>
2025-02-27 23:57:00 -0300

    Improved code readability: formatting and organization.

--- ProjetoPY.py
+++ ProjetoPY.py
@@ -1,48 +1,72 @@
 import pygame
+import Blocks
 from pygame.locals import * #submodolo
 from sys import exit
 import random
 
 pygame.init() #inicializar as funcoes e variaveis do pygame
 
-largura = 1920/2
-altura = 1080/2
-local_objeto = 200,300
-x = largura/2
+LENGHT = 1920/2
+WIDTH = 1080/2
+x = LENGHT/2
 y = 0
-cor = 255,0,0
+RED= 255,0,0
+WHITE = 255,255,255
+KEY_FOR_QUIT = K_ESCAPE
+ITS_MOVING_LEFT = False
+ITS_MOVING_RIGHT = False
 
-tela = pygame.display.set_mode((largura,altura)) #largura e altura da tela
+def Quit():
+    pygame.quit()
+    exit()
+
+def Corners():
+    pygame.draw.rect(screen,(WHITE),(0,0,960,10))
+    pygame.draw.rect(screen,(WHITE),(0,530,960,10))
+    pygame.draw.rect(screen,(WHITE),(0,0,10,960))
+    pygame.draw.rect(screen,(WHITE),(950,0,10,960)) 
+
+screen = pygame.display.set_mode((LENGHT,WIDTH))
 pygame.display.set_caption('Tetris')
-while True: #loop
-    tela.fill((0,0,0))
+while True:
+    screen.fill((0,0,0))
+    Corners()
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-    if event.type == KEYDOWN:
-        if event.key == K_LEFT:
-           x = x - 1
-        if event.key == K_RIGHT:
-           x = x + 1
+     if event.type == KEYDOWN:
+
+        if event.key == K_LEFT and x >= 60:
+            ITS_MOVING_LEFT = True
+
+        if event.key == K_RIGHT and x <= 900:
+            ITS_MOVING = True
+
         if event.key == K_UP:
-           y = y - 1 
+            y = y - 1
+
         if event.key == K_DOWN:
-           y = y + 1
+            y = y + 1 
 
-    #cor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))    
+        if event.key == KEY_FOR_QUIT:
+            Quit()
+
+    if event.type == KEYUP:
+
+        if event.key == K_LEFT:
+            ITS_MOVING_LEFT = False
+
+        if event.key == K_RIGHT:
+            ITS_MOVING = False
+
+    if ITS_MOVING_LEFT:
+        x -= 1/10 
+    if ITS_MOVING_RIGHT:
+        x += 1/10
     #-----------------------------X---Y---L--A)
-    pygame.draw.rect(tela,(cor),(x,y,50,100))
-    if y >= altura:
-        y = 0
-    y = y + 2/5
-
-    #x = x + 1
-    #pygame.draw.rect(tela,(cor),(200,300,100,5))#Reta esquerda
-    #pygame.draw.rect(tela,(cor),(300,300,5,100))#Reta esquerda
-    #pygame.draw.rect(tela,(cor),(200,300,5,100))#Teto
-    #pygame.draw.rect(tela,(cor),(200,400,100,5))#Chao
+    pygame.draw.rect(screen,(RED),(x,y,50,100))
+    
+    if y <= 430:
+        y = y + 1/5
+    y == 430    
 
     pygame.display.update()
     
-    #organizador de estoque,saida e entrada de carga de caminhoes
